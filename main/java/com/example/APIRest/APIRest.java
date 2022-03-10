cb6b84a260c447f14691759546c0c90f7e1b5f24
José Henrique Benemon <96637602+JoseHenriqueBenemon@users.noreply.github.com>
2022-03-10 20:17:41 -0300

    Version 1.0.0 - Concluded

--- main/java/com/example/APIRest/APIRest.java
+++ main/java/com/example/APIRest/APIRest.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication 
-public class PrimeiroProjetoApplication {
+public class APIRest {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PrimeiroProjetoApplication.class, args);
+		SpringApplication.run(APIRest.class, args);
 	}
 
 }
